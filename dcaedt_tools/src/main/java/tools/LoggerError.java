bfeb725c3a83e3edc2cee88ff039a3ceee8716be
Chandan Ghosh <cghosh12@in.ibm.com>
2019-01-29 15:47:18 +0530

    Removed sysout from the class LoggerError
    Removed sysout from the class LoggerError
    
    Issue-ID: DCAEGEN2-1133
    Change-Id: I6a1dc01b99a88f075092083a6bc868bacddccc5d
    Signed-off-by: Chandan Ghosh <cghosh12@in.ibm.com>

--- dcaedt_tools/src/main/java/tools/LoggerError.java
+++ dcaedt_tools/src/main/java/tools/LoggerError.java
@@ -8,11 +8,11 @@ public class LoggerError {
     }
 
     public void log(String logLine) {
-        System.err.println(logLine);
+    	//System.err.println(logLine);
     }
 
     public void log(String logLine, Exception e) {
-        System.err.println(logLine);
+        //System.err.println(logLine);
         e.printStackTrace();
     }
 }
