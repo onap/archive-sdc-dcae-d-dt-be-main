64636c2148414c1fa5da4b46a69570cdab003837
k.kedron <k.kedron@partner.samsung.com>
2019-08-28 14:31:52 +0200

    Fully HTTPS support in the dcaedt-be
    Fully HTTPS support:
    -Updated the onap/base_sdc-jetty docker image version
    -Updated the chef script to properly used of the new docker image
    -Updated jvm configuration to support call to
    the SDC components using HTTPS.
    -Add support for change the http to https in the python script
    -Added buildRestClient method to create the CloseableHttpClient
    supporting the SSL connection
    -Checkstyle in the recipes
    -Update the docker_run.sh:
      - Change JAVA_OPTIONS
      - Used the secure connection to do health check
    
    Issue-ID: SDC-2477
    Signed-off-by: Krystian Kedron <k.kedron@partner.samsung.com>
    Change-Id: I7bf3d307e5765fa75a37ba0a4b41fd7fa87d28ab

--- docker/docker_be/chef-repo/cookbooks/Deploy-DCAE/recipes/dcae_setup.rb
+++ docker/docker_be/chef-repo/cookbooks/Deploy-DCAE/recipes/dcae_setup.rb
@@ -1,6 +1,3 @@
-jetty_base = "#{node['JETTY_BASE']}"
-dcae_logs = "#{node['APP_LOG_DIR']}"
-
 sdc_be_vip = node['BE_VIP']
 dcae_tosca_lab_vip = node['DCAE_TOSCA_LAB_VIP']
 
@@ -17,11 +14,21 @@ else
   dcae_tosca_lab_port = node['DCAE']['TOSCA_LAB']['http_port']
 end
 
-printf("DEBUG: [%s]:[%s] disableHttp=[%s], protocol=[%s], sdc_be_vip=[%s], sdc_be_port=[%s] !!! \n", cookbook_name, recipe_name, node['disableHttp'], protocol, sdc_be_vip ,sdc_be_port )
 
+printf("DEBUG: [%s]:[%s] disableHttp=[%s], protocol=[%s], sdc_be_vip=[%s], sdc_be_port=[%s] !!! \n", cookbook_name, recipe_name, node['disableHttp'], protocol, sdc_be_vip ,sdc_be_port )
 raise "[ERROR] 'SDC_BE_FQDN' is not defined" if sdc_be_vip.nil? || sdc_be_vip == ""
 
-directory "#{jetty_base}/config" do
+
+directory "DCAE BE_tempdir_creation" do
+  path "#{ENV['JETTY_BASE']}/temp"
+  owner 'jetty'
+  group 'jetty'
+  mode '0755'
+  action :create
+end
+
+
+directory "#{ENV['JETTY_BASE']}/config" do
   owner "jetty"
   group "jetty"
   mode '0755'
@@ -29,7 +36,8 @@ directory "#{jetty_base}/config" do
   action :create
 end
 
-directory "#{jetty_base}/config/dcae-be" do
+
+directory "#{ENV['JETTY_BASE']}/config/dcae-be" do
   owner "jetty"
   group "jetty"
   mode '0755'
@@ -37,22 +45,24 @@ directory "#{jetty_base}/config/dcae-be" do
   action :create
 end
 
-directory "#{jetty_base}/config/dcae-be/ves-schema" do
+
+directory "#{ENV['JETTY_BASE']}/config/dcae-be/ves-schema" do
   owner "jetty"
   group "jetty"
   mode '0755'
   recursive true
   action :create
 end
+
 
 template "dcae-be-config" do
   sensitive true
-  path "#{jetty_base}/config/dcae-be/application.properties"
+  path "#{ENV['JETTY_BASE']}/config/dcae-be/application.properties"
   source "dcae-application.properties.erb"
   owner "jetty"
   group "jetty"
   mode "0755"
-  variables ({
+  variables({
     :consumerAuth => node['DCAE']['consumerName'] + ":" + node['DCAE']['consumerPass'] ,
     :dcae_be_port => dcae_be_port,
     :sdc_be_vip => sdc_be_vip,
@@ -63,42 +73,38 @@ template "dcae-be-config" do
   })
 end
 
-cookbook_file "#{jetty_base}/config/dcae-be/ves-schema/CommonEventFormat_v4.1.json" do
+
+cookbook_file "#{ENV['JETTY_BASE']}/config/dcae-be/ves-schema/CommonEventFormat_v4.1.json" do
   source "CommonEventFormat_v4.1.json"
   owner "jetty"
   group "jetty"
   mode 0755
 end
 
-cookbook_file "#{jetty_base}/config/dcae-be/ves-schema/CommonEventFormat_v5.3.json" do
+
+cookbook_file "#{ENV['JETTY_BASE']}/config/dcae-be/ves-schema/CommonEventFormat_v5.3.json" do
   source "CommonEventFormat_v5.3.json"
   owner "jetty"
   group "jetty"
   mode 0755
 end
 
+
 template "dcae-error-configuration" do
   sensitive true
-  path "#{jetty_base}/config/dcae-be/error-configuration.yaml"
+  path "#{ENV['JETTY_BASE']}/config/dcae-be/error-configuration.yaml"
   source "dcae-error-configuration.erb"
   owner "jetty"
   group "jetty"
   mode "0755"
 end
 
+
 template "dcae-logback-spring-config" do
   sensitive true
-  path "#{jetty_base}/config/dcae-be/logback-spring.xml"
+  path "#{ENV['JETTY_BASE']}/config/dcae-be/logback-spring.xml"
   source "dcae-logback-spring.erb"
   owner "jetty"
   group "jetty"
   mode "0755"
-end
-
-directory "#{dcae_logs}" do
-  owner "jetty"
-  group "jetty"
-  mode '0755'
-  recursive true
-  action :create
 end
--- docker/docker_be/startup.sh
+++ docker/docker_be/startup.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
-set -x 
-# Run chef-solo for configuration
-cd /var/opt/dcae-be/chef-solo
 
-chef-solo -c solo.rb -E ${ENVNAME} --log_level "debug" --logfile "/tmp/Chef-Solo.log"
+JAVA_OPTIONS=" ${JAVA_OPTIONS} -Dconfig.home=${JETTY_BASE}/config \
+               -Dlog.home=${JETTY_BASE}/logs \
+               -Dlogback.configurationFile=${JETTY_BASE}/dcae-be/logback-spring.xml
+               -Djavax.net.ssl.trustStore=${JETTY_BASE}/etc/org.onap.sdc.trust.jks \
+               -Djavax.net.ssl.trustStorePassword=Y,f975ZNJfVZhV*{+Y[}pA?0 \
+               -Djetty.console-capture.dir=${JETTY_BASE}/logs"
+
+cd /root/chef-solo
+chef-solo -c solo.rb -E ${ENVNAME}
 
 status=$?
-if [ $status != 0 ]; then
+if [[ ${status} != 0 ]]; then
   echo "[ERROR] Problem detected while running chef. Aborting !"
   exit 1
 fi
 
-# Execute Jetty
 cd /var/lib/jetty
 /docker-entrypoint.sh &
 
